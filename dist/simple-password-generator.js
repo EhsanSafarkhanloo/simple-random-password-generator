cea87ed9d604eb0039e86027663af2fd9231490a
Ehsan Safarkhanloo <107636462+EhsanSafarkhanloo@users.noreply.github.com>
2024-05-14 13:22:41 +0330

    simple random password generator

---
+++ dist/simple-password-generator.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function generateRandomPassword({ length = 8, includeNumbers = true, includeLowercase = true, includeUppercase = true, includeSpecialChars = true, }) {
+    let charset = "";
+    if (includeNumbers) {
+        charset += "0123456789";
+    }
+    if (includeLowercase) {
+        charset += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (includeUppercase) {
+        charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (includeSpecialChars) {
+        charset += "!@#$%^&*()_+{}[]|;:<>,.?/";
+    }
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+exports.default = generateRandomPassword;
---
+++ src/simple-password-generator.ts
@@ -0,0 +1,27 @@
+export default function generateRandomPassword({
+  length = 8,
+  includeNumbers = true,
+  includeLowercase = true,
+  includeUppercase = true,
+  includeSpecialChars = true,
+}): string {
+  let charset = "";
+  if (includeNumbers) {
+    charset += "0123456789";
+  }
+  if (includeLowercase) {
+    charset += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (includeUppercase) {
+    charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (includeSpecialChars) {
+    charset += "!@#$%^&*()_+{}[]|;:<>,.?/";
+  }
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+  return password;
+}
